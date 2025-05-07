bbfc716c33e4d36165aaeabbeb1498837f843a0a
temp-dev1 <temp0xd@proton.me>
2025-05-07 16:51:22 +0530

    Update certpartner-form.tsx

--- components/admin/certpartner-form.tsx
+++ components/admin/certpartner-form.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Loader2 } from 'lucide-react';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import Image from 'next/image';
 
 interface CertPartnerFormProps {
